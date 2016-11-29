19f1c702e89d5ee868293b3d7dc20254200bd5a5
mickel.mcmillan <mickel.mcmillan@my.uwi.edu>
2016-11-29 18:50:13 -0400

    Displaying modal when a user attempts to make a request to send suggested item for trade with the requested item; incomplete

--- templates/homepage.php
+++ templates/homepage.php
@@ -16,7 +16,7 @@ include "base.php";
   <table class="table table-hover table-condensed">
     <thead class="thead-inverse">
       <tr>
-        <th>Image</th><th>Name</th><th>Description</th><th>Trader</th><th>Trade</th><th>Uploaded</th><th></th>
+        <th>Image</th><th>Name</th><th>Description</th><th>Trader</th><th>Trade</th><th>Uploaded</th>
       </tr>
     </thead>
     <tbody>
@@ -27,22 +27,21 @@ include "base.php";
     <div class="modal-dialog modal-sm">
       <div class="modal-content">
         <div class="modal-body">
-          <form class="form-vertical">
+          <form class="form-vertical" method="POST" onsubmit="return sendId();">
             <fieldset>
             <!-- Select Basic -->
             <div class="form-group">
               <label class="col-md-8 control-label" for="selectbasic">Select Your Item</label>
               <div class="col-md-8">
-                <select id="selectbasic" name="selectbasic" class="form-control">
-                  <option value="1">Option one</option>
-                  <option value="2">Option two</option>
+                <select id="items" name="itemid" class="form-control">
+                  <option value="0">Select Item</option>
                 </select>
               </div>
             </div>
 
             <div class="form-group">
               <div class="col-md-4">
-                <button id="sendrequest" name="sendrequest" class="btn btn-success" type="submit">Send</button>
+                <button id="sendrequest" name="sendrequest" class="btn btn-success" type="submit">Send Request</button>
                 <button id="cancelrequest" name="cancelrequest" class="btn btn-danger" data-dismiss="modal">Cancel</button>
               </div>
             </div>
