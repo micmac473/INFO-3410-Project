a0af745d54b0ae708bc45bb894d205d510e565ed
I-am-sham <shamarculzac@gmail.com>
2016-11-30 00:13:10 -0400

    Update valscript.js

--- js/valscript.js
+++ js/valscript.js
@@ -6,12 +6,6 @@
 
 		// function to submit the form after all validation has occurred			
 		$scope.submitForm = function() {
-
-			// check to make sure the form is completely valid
-			if ($scope.userForm.$valid) {
-				alert('Thank you for signing up');
-			}
-
 		};
 
 	});
